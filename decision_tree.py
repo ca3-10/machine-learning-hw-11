468bfb9359eed68cfc494d371137d4b2f357145f
ca3-10 <89424776+ca3-10@users.noreply.github.com>
2022-12-30 20:48:59 +0000

    not a connected tree but has the correct splits

--- decision_tree.py
+++ decision_tree.py
@@ -1,3 +1,16 @@
+class Stack:
+    def __init__(self):
+        self.elements = []
+
+    def print(self):
+        print(self.elements)
+
+    def push(self, value):
+        self.elements.append(value)
+
+    def pop(self):
+        self.elements.pop()
+
 class Node: 
     def __init__(self, data_points): 
         self.data_points = data_points
@@ -6,7 +19,22 @@ class Node:
         self.shortbread_counts = self.get_shortbread_counts(self.data_points)
         self.sugar_counts = self.num_data_points - self.shortbread_counts
 
+        self.pure = False
+        if self.shortbread_counts == self.num_data_points or self.sugar_counts == self.num_data_points:
+            self.pure = True
+            return
+        
         self.g_before = self.gini_impurity_before()
+
+        self.best_split = self.best_split()
+
+        #left = points greater than equal to the best split
+        self.left_data = self.point_counts(self.best_split[1], self.best_split[0], '<=')
+
+        #right = points less than the best split
+        self.right_data = self.point_counts(self.best_split[1], self.best_split[0], '>')
+
+
     
     def get_shortbread_counts(self, array):
         shortbread_counts = 0
@@ -56,6 +84,8 @@ class Node:
             y_impurities = {}
 
             for x in x_splits: 
+                if len(x_splits) == 0: 
+                    break
                 points_less_than_equal = self.point_counts(x, 'x', '<=')
                 points_greater_than = self.point_counts(x, 'x', '>')
 
@@ -71,6 +101,8 @@ class Node:
                 x_impurities[x] = self.g_before - g_after
             
             for y in y_splits: 
+                if len(y_splits) == 0: 
+                    break
                 points_less_than_equal = self.point_counts(y, 'y', '<=')
                 points_greater_than = self.point_counts(y, 'y', '>')
 
@@ -85,18 +117,46 @@ class Node:
 
                 y_impurities[y] = self.g_before - g_after
 
-            best_x_split = max(x_impurities, key=x_impurities.get)
-            best_y_split = max(y_impurities, key=y_impurities.get)
-            
-            if x_impurities[best_x_split] > y_impurities[best_y_split]:
-                return ('x', best_x_split)
-            else:
+
+            if len(x_splits) == 0:
+                best_y_split = max(y_impurities, key=y_impurities.get)
                 return ('y', best_y_split)
-                
+            elif len(y_splits) == 0:
+                best_x_split = max(x_impurities, key=x_impurities.get)
+                return ('x', best_x_split)
+            else: 
+                best_x_split = max(x_impurities, key=x_impurities.get)
+                best_y_split = max(y_impurities, key=y_impurities.get)
+            
+                if x_impurities[best_x_split] > y_impurities[best_y_split]:
+                    return ('x', best_x_split)
+                else:
+                    return ('y', best_y_split)
+                    
+class DecisionTree:
+    def __init__(self, data_points):
+        self.data_points = data_points
+        self.root = Node(data_points)
         
-        
+    
+    def build_tree(self):
+        stack = Stack()
+        stack.push(self.root)
+        node_splits = {}
+        while len(stack.elements) != 0:
+            current_node = stack.elements[-1]
+            if current_node.pure == True: 
+                continue
+            node_splits[current_node] = current_node.best_split
+            stack.pop()
 
+            children = [Node(current_node.left_data), Node(current_node.right_data)]
 
+            for child in children: 
+                if child.pure == True: 
+                    continue
+                stack.push(child)
+        return node_splits
 
 data = [['Shortbread',0.15,0.2],
         ['Shortbread',0.15,0.3],
@@ -108,7 +168,12 @@ data = [['Shortbread',0.15,0.2],
         ['Sugar',0.1,0.3],
         ['Sugar',0.15,0.4],
         ['Sugar',0.25,0.35]]
-    
+
 node = Node(data)
-print(node.shortbread_counts)
-print(node.best_split())
+tree = DecisionTree(data)
+p= tree.build_tree()
+for node in p: 
+    print(p[node])
+    print(node.data_points)
+    print()
+
