6be44eff0b939da3a1a746f08a2117ddc64f720d
ca3-10 <riseredasthedawn926@gmail.com>
2023-05-01 22:41:25 +0000

    more gradient descent components

--- backpropagation.py
+++ backpropagation.py
@@ -2,23 +2,15 @@ import numpy as np
 import math
 
 class BackProp: 
-    def __init__(self, point): 
+    def __init__(self, A, b, point): 
         self.x = np.array([point[0]])
         self.y = np.array([point[1]])
 
-        self.A_1 = np.array([[5], [-5], [5], [-5]])
-        self.A_2 = np.array([[10,10,0,0], [0,0,10,10]])
-        self.A_3 = np.array([[10,10]])
-
-        self.b_1 = np.array([[-0.75], [1.75], [-3.25], [4.25]])
-        self.b_2 = np.array([[-12.5], [-12.5]])
-        self.b_3 = np.array([[-2.5]])
-
-        self.A = [self.A_1, self.A_2, self.A_3]
-        self.b = [self.b_1, self.b_2, self.b_3]
+        self.A = A
+        self.b = b
         self.input = [self.x]
         self.h = [self.x]
-    
+
     def sigmoid(self,input): 
     
         new = [[1/ (1 + math.e ** (-float(value[0])))] for value in input]
@@ -59,35 +51,80 @@ class BackProp:
         self.back_prop()
 
         self.rss_b = []
-        self.rss_a = []
+        self.rss_A = []
 
 
         for i in range(3): 
             self.rss_b.append(self.rss_h[2-i] * self.sigmoid_prime(self.input[3-i]))
-            self.rss_a.append(self.outer_product(self.rss_b[i], self.h[2-i]))
+            self.rss_A.append(self.outer_product(self.rss_b[i], self.h[2-i]))
         
             #print('b',self.rss_b[i])
             #print('a', self.rss_a[i])
             #print()
+        return {'rss_A' : self.rss_A, 'rss_b' : self.rss_b}
 
 class GradientDescent: 
-    def __init__(self, points): 
+
+    def __init__(self, points, intial_A, intial_b): 
         self.points = points
+        self.A = intial_A
+        self.b = intial_b
+
+        self.alpha = np.array([[0.01]])
     
-    def sum(self): 
-        total = 0
-        for point in self.points: 
-            prop = BackProp(point)
+    def gradient_sums(self): 
+        summed_rss_A = []
+        summed_rss_b = []
+
+
+        for i in range(len(self.points)): 
+
+            prop = BackProp(self.A, self.b, self.points[i])
             prop.weight_gradients()
-            print(prop.rss_b[0])
-            total += prop.rss_b[0]
-        print(total)
+
+
+            if i == 0: 
+                for i in range(len(prop.rss_A)): 
+                    summed_rss_A.append(prop.rss_A[i])
+                    summed_rss_b.append(prop.rss_b[i])
+
+                continue
+        
+            for j in range(len(prop.A)): 
+                summed_rss_A[j] += prop.rss_A[j]
+                summed_rss_b[j] += prop.rss_b[j]
+        
+        return summed_rss_A, summed_rss_b
+    
+    def gradient_descent(self):
+        weight_A, weight_b = self.gradient_sums()
+        weight_A.reverse()
+        weight_b.reverse()
+        
+        updated_A = []
+        updated_b = []
+
+        new_weight = np.array([[weight_A[0][i]] for i in range(4)])
+        weight_A[0] = new_weight
+
+        for i in range(len(weight_A)):
+            updated_b.append(self.b[i] - (self.alpha * weight_b[i]))
+            updated_A.append(self.A[i] - self.alpha * weight_A[i])
+        
+        #print(updated_A)
+        print(updated_b)
+
+
+    
 
 
 
 points = [[0,0], [0.25, 1], [0.5, 0.5], [0.75, 1], [1,0]]
+A = [np.array([[5], [-5], [5], [-5]]), np.array([[10,10,0,0], [0,0,10,10]]), np.array([[10,10]])]
+B = [np.array([[-0.75], [1.75], [-3.25], [4.25]]), np.array([[-12.5], [-12.5]]), np.array([[-2.5]])]
 
-grad = GradientDescent(points)
-grad.sum()
+grad = GradientDescent(points, A, B)
+grad.gradient_descent()
+
 
 
