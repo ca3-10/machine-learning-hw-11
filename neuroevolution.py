9de4b02eb557faee652077afa49f8bb0f2e072be
ca3-10 <riseredasthedawn926@gmail.com>
2023-05-23 22:00:52 +0000

    cleaned up code anded helper function

--- neuroevolution.py
+++ neuroevolution.py
@@ -27,8 +27,9 @@ class Node:
         return  ((math.e ** (x)) - (math.e ** (-x))) / ((math.e ** (x)) + (math.e ** (-x)))
     
 class BiasNode: 
-    def __init__(self): 
+    def __init__(self, num_weights): 
         self.value = 1
+        self.num_weights = self.generate_children_weights(num_weights)
     
     def generate_children_weights(self, num_weights): 
         self.children_weights = [1 for i in range(num_weights)]
@@ -40,58 +41,40 @@ class NeuralNet:
         self.bias_node = BiasNode(10)
         self.actual_output = input_point[1]
     
-    def build_layer(self, incoming_layer, next_layer_num): 
+    def build_layer(self, incoming_layer, next_layer_num):
+        
+        #create the layer 
+        next_layer = [Node(0) for i in range(next_layer_num)]
 
+        #generate parent weights
         for nodes in incoming_layer: 
             nodes.generate_children_weights(next_layer_num)
         
-        next_layer = [Node(0) for i in range(next_layer_num)]
-
+        #calculate values
         for i in range(len(next_layer)): 
             current_node = next_layer[i]
             current_node.value = current_node.calc_value(incoming_layer, i)
+            current_node.generate_children_weights(next_layer_num)
         
-        
+        #add bias node
+        if next_layer_num != 1:
+            next_layer.append(BiasNode(next_layer_num))
 
-        
+        return next_layer
     
     def build_net(self): 
         self.input_layer = [self.input_node, self.bias_node]
 
-        for nodes in self.input_layer: 
-            self.input_node.generate_children_weights(10)
+        self.first_hidden_layer = self.build_layer(self.input_layer, 10)      
 
-        self.first_hidden_layer = [Node(0) for i in range(10)]        
+        self.second_hidden_layer = self.build_layer(self.first_hidden_layer, 6)
 
-        for i in range(len(self.first_hidden_layer)): 
-            current_node = self.first_hidden_layer[i]
-            current_node.value = current_node.calc_value(self.input_layer, i)
-            current_node.generate_children_weights(6)
-        
-        self.first_hidden_layer.append(BiasNode(6))
-        self.second_hidden_layer = [Node(0) for i in range(6)]
+        self.third_hidden_layer = self.build_layer(self.second_hidden_layer, 3)
 
-        for i in range(len(self.second_hidden_layer)): 
-            current_node = self.second_hidden_layer[i]
-            current_node.value = current_node.calc_value(self.first_hidden_layer,i)
-            current_node.generate_children_weights(3)
-        
-        self.second_hidden_layer.append(BiasNode(3))
-        self.third_hidden_layer = [Node(0) for i in range(3)]
+        self.output_layer = self.build_layer(self.third_hidden_layer, 1)
+    
 
-        for i in range(len(self.third_hidden_layer)): 
-            current_node = self.third_hidden_layer[i]
-            current_node.value = current_node.calc_value(self.second_hidden_layer, i)
-            current_node.generate_children_weights(1)
-        
-        self.third_hidden_layer.append(BiasNode(1))
-        self.output_layer = [Node(0)]
-
-        for i in range(len(self.output_layer)): 
-            current_node = self.output_layer[i]
-            current_node.value = current_node.calc_value(self.third_hidden_layer, i)
-        
-        print(current_node.value)
+        print(self.output_layer[0].value)
             
         
         
